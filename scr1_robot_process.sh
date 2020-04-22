be0e5b061fd7e7256e0d404f74c0b76fe5d3a00e
Tomualdo <tomas.krajcoviech@gmail.com>
2020-04-22 21:08:54 +0200

    first commit

--- scr1_robot_process.sh
+++ scr1_robot_process.sh
@@ -6,9 +6,5 @@ if ps -ef | grep -v grep | grep scr1_robot_loger.py ; then
 else
 		echo not running
         /home/tom/Projects/proface/scr1_logger/scr1_robot_loger.py
-
-        #/home/user/bin/doctype.php >> /home/user/bin/spooler.log &
-        #mailing program
-        #/home/user/bin/simplemail.php "Print spooler was not running...  Restarted." 
         exit 0
 fi
