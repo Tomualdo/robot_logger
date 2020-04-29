b2c85da60e029fa417c44d147645052bd6e6c2c7
Tomualdo <tomas.krajcoviech@gmail.com>
2020-04-29 22:24:45 +0200

    added 3d ploting just for fun

---
+++ test.py
@@ -0,0 +1,28 @@
+import re
+from mpl_toolkits import mplot3d
+import numpy as np
+from numpy  import array
+import matplotlib.pyplot as plt
+with open("0001.JOB") as fjob:
+    job = fjob.read()
+#print (job)
+x=[]
+y=[]
+z=[]
+for line in job.splitlines():
+    if(line.startswith("S")):
+        doRegex = re.search ("\(([-0-9]+.[0-9]+),([-0-9]+.[0-9]+),([-0-9]+.[0-9]+)",line)
+        print ("X: ",doRegex.group(1),"Y: ",doRegex.group(2),"Z: ",doRegex.group(3))
+        x.append(float(doRegex.group(1)))
+        y.append(float(doRegex.group(2)))
+        z.append(float(doRegex.group(3)))
+
+print ("x: ",x,"y: ",y,"z: ",z)
+x=array(x)
+y=array(y)
+z=array(z)
+print ("x: ",x,"y: ",y,"z: ",z)
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot3D(x, y, z, 'gray')
+plt.show()
